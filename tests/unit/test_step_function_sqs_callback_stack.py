c10e59a3f8fc10e9b92c361013ed02f8ff4f4a8b
takashi <yagita.takashi@gmail.com>
2022-09-14 16:55:56 +0900

    Initial commit

---
+++ tests/unit/test_step_function_sqs_callback_stack.py
@@ -0,0 +1,15 @@
+import aws_cdk as core
+import aws_cdk.assertions as assertions
+
+from step_function_sqs_callback.step_function_sqs_callback_stack import StepFunctionSqsCallbackStack
+
+# example tests. To run these tests, uncomment this file along with the example
+# resource in step_function_sqs_callback/step_function_sqs_callback_stack.py
+def test_sqs_queue_created():
+    app = core.App()
+    stack = StepFunctionSqsCallbackStack(app, "step-function-sqs-callback")
+    template = assertions.Template.from_stack(stack)
+
+#     template.has_resource_properties("AWS::SQS::Queue", {
+#         "VisibilityTimeout": 300
+#     })
